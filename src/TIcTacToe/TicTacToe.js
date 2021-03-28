b5ed8e29908f2c3364aa779cdd824dfb3839107c
beydulla <zeynalov.beydulla97@gmail.com>
2021-03-28 23:13:48 +0200

    green -> 3x3 board array

--- src/TIcTacToe/TicTacToe.js
+++ src/TIcTacToe/TicTacToe.js
@@ -2,6 +2,13 @@ class TicTacToe {
     constructor() {
         this.playerX = 'X';
         this.playerO = 'O';
+        this.board = this.initBoard();
+    }
+
+    initBoard = () => {
+     return [[' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']];
     }
 }
 
