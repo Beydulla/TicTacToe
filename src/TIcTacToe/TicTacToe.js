0cc4375ed0b9482a865454a6b4ef770b1b545f00
beydulla <zeynalov.beydulla97@gmail.com>
2021-03-28 23:21:52 +0200

    refactor

--- src/TIcTacToe/TicTacToe.js
+++ src/TIcTacToe/TicTacToe.js
@@ -6,11 +6,10 @@ class TicTacToe {
     }
 
     drawBoard = () => {
-        return this.board[0][0] + "|" + this.board[0][1] + "|" + this.board[0][2] + "\n" +
-            "-+-+-\n" +
-            this.board[1][0] + "|" + this.board[1][1] + "|" + this.board[1][2] + "\n" +
-            "-+-+-\n" +
-            this.board[2][0] + "|" + this.board[2][1] + "|" + this.board[2][2];
+        const board = this.board;
+        return  board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "\n-+-+-\n" +
+                board[1][0] + "|" + board[1][1] + "|" + board[1][2] + "\n-+-+-\n" +
+                board[2][0] + "|" + board[2][1] + "|" + board[2][2];
     }
 }
 
