e1df65ca1d634865a19f5604978dff53875222c1
beydulla <zeynalov.beydulla97@gmail.com>
2021-03-28 23:20:06 +0200

    green -> drawing empty board

--- src/TIcTacToe/TicTacToe.js
+++ src/TIcTacToe/TicTacToe.js
@@ -5,8 +5,14 @@ class TicTacToe {
         this.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     }
 
+    drawBoard = () => {
+        return this.board[0][0] + "|" + this.board[0][1] + "|" + this.board[0][2] + "\n" +
+            "-+-+-\n" +
+            this.board[1][0] + "|" + this.board[1][1] + "|" + this.board[1][2] + "\n" +
+            "-+-+-\n" +
+            this.board[2][0] + "|" + this.board[2][1] + "|" + this.board[2][2];
+    }
 }
-
 
 module.exports = {
     TicTacToe
