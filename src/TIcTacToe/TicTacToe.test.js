4e32e19aac426f7f31ecc9ac32befcd4540af3d9
beydulla <zeynalov.beydulla97@gmail.com>
2021-03-29 00:18:23 +0200

    red -> winner with left diagonal

--- src/TIcTacToe/TicTacToe.test.js
+++ src/TIcTacToe/TicTacToe.test.js
@@ -74,5 +74,10 @@ describe("Tests TicTacToe Game", () => {
             game.board = [['X', 'O', ' '], ['O', 'X', 'O'], ['X', ' ', 'X']];
             expect(game.isWinner("X")).toEqual(true);
         });
+        test("Expect Player O to win with left diagonal line", () => {
+            const game = new TicTacToe();
+            game.board = [['X', ' ', 'O'], [' ', 'O', 'X'], ['O', ' ', ' ']];
+            expect(game.isWinner("O")).toEqual(true);
+        });
     });
 })
